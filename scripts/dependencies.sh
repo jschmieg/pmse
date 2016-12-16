de733b298df221c312a3f748157533caa2a37bf4
Jakub Schmiegel <jakub.schmiegel@intel.com>
2016-12-16 13:15:33 +0100

    Travis test12

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-sudo apt-get install pandoc
-sudo apt-get install doxygen
-git clone https://github.com/pmem/nvml.git
-cd nvml
-make
-sudo make install
-cd ..
+#sudo apt-get install pandoc
+#sudo apt-get install doxygen
+pwd
+ls
+#git clone https://github.com/pmem/nvml.git
+#cd nvml
+#make
+#sudo make install
+#cd ..
 git clone https://github.com/mongodb/mongo.git
 cd mongo
 mkdir -p src/mongo/db/modules/
+pwd
+ls
+
 ln -sf ~/pmse src/mongo/db/modules/pmse
 ls src/mongo/db/modules/pmse
 
