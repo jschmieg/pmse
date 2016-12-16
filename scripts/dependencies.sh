f72f224ead3a3e1a8b0fc911d1441b38df9f8cda
Jakub Schmiegel <jakub.schmiegel@intel.com>
2016-12-16 13:04:25 +0100

    Travis test10

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -10,5 +10,5 @@ git clone https://github.com/mongodb/mongo.git
 cd mongo
 mkdir -p src/mongo/db/modules/
 ln -sf ~/pmse src/mongo/db/modules/pmse
-ls mongo/src/mongo/db/modules/
+ls src/mongo/db/modules/
 
