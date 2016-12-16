7481997f9175731c79de8cd904ec0f3449d1f3b5
Jakub Schmiegel <jakub.schmiegel@intel.com>
2016-12-16 14:54:36 +0100

    Travis test24

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,6 +2,12 @@
 #sudo apt-get install scons
 wget http://mirrors.kernel.org/ubuntu/pool/main/s/scons/scons_2.3.0-2_all.deb
 sudo dpkg -i scons_2.3.0-2_all.deb
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+
+sudo apt-get install -qq g++-5.3
+export CXX="g++-5.3"
+
 #sudo apt-get install pandoc
 #sudo apt-get install doxygen
 pwd
