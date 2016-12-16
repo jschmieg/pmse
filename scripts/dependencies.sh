0af1af1f8c1ff961b81deb32b12b61fc05a0e892
Jakub Schmiegel <jakub.schmiegel@intel.com>
2016-12-16 14:57:18 +0100

    Travis test25

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,7 +5,7 @@ sudo dpkg -i scons_2.3.0-2_all.deb
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
-sudo apt-get install -qq g++-5.3
+sudo apt-get install -qq g++-5
 export CXX="g++-5.3"
 
 #sudo apt-get install pandoc
